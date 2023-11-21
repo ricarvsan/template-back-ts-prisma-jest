d1e00beaa263724fe1b91a0c01edac0618471847
Ricardo Carvalho dos Santos <ricarvsan@gmail.com>
2023-11-21 14:24:39 -0300

    build: build without seed v2

--- dist/src/routers/messages-router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var messages_controller_1 = require("../controllers/messages-controller");
-var messageRouter = (0, express_1.Router)();
-messageRouter.get('/', messages_controller_1.getAllMessages);
-exports.default = messageRouter;
